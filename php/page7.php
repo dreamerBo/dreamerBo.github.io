393b50633767337e8a43f6e9a8d9a1ac1a79162a
dreamerBo <mars13901964@hotmail.com>
2016-01-06 23:03:58 +1100

    2

---
+++ php/page7.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Hire Me</title>
+</head>
+<body>
+	<form action="./main.php" method="post">
+		<input type="text" value="999">
+
+		<input type="submit" value="submit">
+	</form>	
+</body>
+</html>
